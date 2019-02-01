faf9c1450817be7d09f6fc4c70f24a4cd30f28e9
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-01 18:48:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,15 @@ def oxford_comma(array)
   array.insert(3, ", ")
   array.join
 else
-  array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+  array.insert(1, ", ")
+  array.insert(3,", ")
+  array.insert(5, ", ")
+  array.insert(7, ", ")
+  array.insert(8, "and ")
+  #array.insert(9, " ")
+  #array.insert(9, ", ")
+  #array.insert(11, ", ")
+  #array.insert(13, ", ")
+  array.join
   end
 end
